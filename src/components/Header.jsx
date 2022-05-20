245e46740ffc310f5ebf830576328514b40433fd
leenahiq <97670794+leenahiq@users.noreply.github.com>
2022-05-20 14:58:32 +0100

    Updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
 import CV from "../assets/cv.pdf";
+import "../components/header.css";
+// import ME from "../assets/img.png";
 import { BsLinkedin, BsGithub, BsTwitter } from "react-icons/bs";
 const Header = () => {
   return (
     <header>
-      <div className="container header_container">
+      <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Leenah Iqbal</h1>
         <h5 className="text-light">full Stack Developer</h5>
         <div>
           <h5>I love designing and building web applications </h5>
         </div>
-        <div className="ctm">
+        <div className="cta">
           <a
             href={CV}
             download
@@ -24,19 +26,25 @@ const Header = () => {
             Let's Talk
           </a>
         </div>
-        <a href="#contact" className="scroll_down">
+
+        <div className="header__socials">
+          <a href="/" target="_blank" rel="noreferrer">
+            <BsLinkedin />
+          </a>
+          <a href="/" target="_blank" rel="noreferrer">
+            <BsGithub />
+          </a>
+          <a href="/" target="_blank" rel="noreferrer">
+            <BsTwitter />
+          </a>
+        </div>
+
+        {/* <div className="me">
+          <img src={ME} alt="img" />
+        </div> */}
+        {/* <a href="#contact" className="scroll_down">
           Contact
-        </a>
-        <div className="header__socials"></div>
-        <a href="/" target="_blank" rel="noreferrer">
-          <BsLinkedin />
-        </a>
-        <a href="/" target="_blank" rel="noreferrer">
-          <BsGithub />
-        </a>
-        <a href="/" target="_blank" rel="noreferrer">
-          <BsTwitter />
-        </a>
+        </a> */}
       </div>
     </header>
   );
