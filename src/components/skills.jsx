a57cdaaf03f1a652e54eaa1952d7064f78f897e0
leenahiq <97670794+leenahiq@users.noreply.github.com>
2022-05-22 16:17:04 +0100

    updated home, about and added skill page

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,5 +1,89 @@
+import { BsPatchCheckFill } from "react-icons/bs";
+import "../components/skills.css";
 const Skills = () => {
-  return <section id="skills">skills</section>;
+  return (
+    <section id="skills">
+      <h2>Skills</h2>
+
+      <div className="container experience_container">
+        <div className="experience_frontend">
+          <h3>Frontend Developement</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BsPatchCheckFill className="icon" />
+              <h4>HTML5</h4>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="icon" />
+              <h4>CSS3</h4>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="icon" />
+              <h4>Bootstrap</h4>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="icon" />
+              <h4>JavaScript</h4>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="icon" />
+              <h4>React</h4>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="icon" />
+              <h4>Figma</h4>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="icon" />
+              <h4>Netlify</h4>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="icon" />
+              <h4>GitHub</h4>
+            </article>
+          </div>
+        </div>
+        <div className="experience_backend">
+          <h3>backend Developement</h3>
+
+          <div className="experience_content">
+            <article className="experience_details">
+              <BsPatchCheckFill className="icon" />
+              <h4>Node.js</h4>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="icon" />
+              <h4>MongoDB</h4>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="icon" />
+              <h4>Mongoose</h4>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="icon" />
+              <h4>SQL</h4>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="icon" />
+              <h4>Squelise</h4>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="icon" />
+              <h4>Express.js</h4>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="icon" />
+              <h4>Heroku</h4>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="icon" />
+              <h4>GitHub</h4>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Skills;
