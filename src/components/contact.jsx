65e21e8ebbcea284ccb43ce1831a2df5a23f7823
leenahiq <97670794+leenahiq@users.noreply.github.com>
2022-05-23 17:38:42 +0100

    added contact details

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,6 +1,49 @@
+import "../components/contact.css";
+import { MdEmail } from "react-icons/md";
+import { BsLinkedin, BsTwitter } from "react-icons/bs";
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <h2>Get In Touch</h2>
+
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className="contact_option">
+            <MdEmail />
+            <h4>Email</h4>
+            <h5>Have you got some questions?</h5>
+            <a href="leeangel_huzy@icloud.com">send a message</a>
+          </article>
+          <article className="contact_option">
+            <BsLinkedin />
+            <h4>LinkedIn</h4>
+            <h5>Stay connected or have a liitle chat!</h5>
+
+            <a href="https://www.linkedin.com/messaging/">send a message</a>
+          </article>
+          <article className="contact_option">
+            <BsTwitter />
+            <h4>Twitter</h4>
+            <h5>Follow me or drop me a message!</h5>
+            <a href="https://twitter.com/messages">send a message</a>
+          </article>
+        </div>
+        <form action="">
+          <input type="text" name="name" placeholder="Full Name" required />
+          <input type="email" name="email" placeholder="Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Message"
+            required
+          ></textarea>
+          <button className="btn btn-primary" type="submit">
+            Send
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
-<div></div>;
