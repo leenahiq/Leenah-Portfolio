f30d48dd522b9da422cb76f65b8ca51c1dbc508e
leenahiq <97670794+leenahiq@users.noreply.github.com>
2022-05-23 18:07:39 +0100

    added footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,25 @@
+import "../components/footer.css";
+import { BiCopyright } from "react-icons/bi";
+import { BsLinkedin, BsGithub, BsTwitter } from "react-icons/bs";
 const Footer = () => {
   return (
-    <div>
-      <div>Footer</div>
-      <a href="#home">back to home page</a>
-    </div>
+    <footer>
+      <h3>
+        Leenah Iqbal
+        <BiCopyright />
+      </h3>
+      <div className="footer_icon">
+        <a href="/" target="_blank">
+          <BsLinkedin />
+        </a>
+        <a href="/" target="_blank">
+          <BsGithub />
+        </a>
+        <a href="/" target="_blank">
+          <BsTwitter />
+        </a>
+      </div>
+    </footer>
   );
 };
 
