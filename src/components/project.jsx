685b78834111e8a96b49eb523a775f26d3878221
leenahiq <97670794+leenahiq@users.noreply.github.com>
2022-05-23 11:34:16 +0100

    updated images sizes in portfolio page

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,5 +1,5 @@
 import apple from "../assets/apple.png";
-import recipe from "../assets/recipe.png";
+// import recipe from "../assets/recipe.png";
 import insta from "../assets/insta.jpg";
 import dice from "../assets/dice.png";
 import list from "../assets/list.png";
@@ -10,10 +10,24 @@ const Project = () => {
   return (
     <section id="projects">
       <h2>Portfolio</h2>
+      <div>
+        {/* <article className="portfolio_items">
+          <div className="portfolio_item-image">
+            <img className="" src={recipe} alt="recipe site" />
+          </div>
+          <h3>project 1</h3>
+          <a href="/" className="btn">
+            gitHub
+          </a>
+          <a href="/" className="btn btn-primary">
+            Live Demo
+          </a>
+        </article> */}
+      </div>
       <div className="container portfolio_container">
-        <article className="portfolio_items">
+        <article className="portfolio_item">
           <div className="portfolio_item-image">
-            <img src={recipe} alt="recipe site" />
+            <img className="img" src={apple} alt="apple clone" />
           </div>
           <h3>project 1</h3>
           <a href="/" className="btn">
@@ -25,7 +39,7 @@ const Project = () => {
         </article>
         <article className="portfolio_item">
           <div className="portfolio_item-image">
-            <img src={apple} alt="apple clone" />
+            <img className="img" src={insta} alt="insta clone" />
           </div>
           <h3>project 1</h3>
           <a href="/" className="btn">
@@ -37,7 +51,7 @@ const Project = () => {
         </article>
         <article className="portfolio_item">
           <div className="portfolio_item-image">
-            <img src={insta} alt="insta clone" />
+            <img className="img" src={dice} alt="dice game" />
           </div>
           <h3>project 1</h3>
           <a href="/" className="btn">
@@ -49,7 +63,7 @@ const Project = () => {
         </article>
         <article className="portfolio_item">
           <div className="portfolio_item-image">
-            <img src={dice} alt="dice game" />
+            <img className="img" src={list} alt="to-do-list" />
           </div>
           <h3>project 1</h3>
           <a href="/" className="btn">
@@ -61,7 +75,7 @@ const Project = () => {
         </article>
         <article className="portfolio_item">
           <div className="portfolio_item-image">
-            <img src={list} alt="to-do-list" />
+            <img className="img" src={poke} alt="poke-search-bar" />
           </div>
           <h3>project 1</h3>
           <a href="/" className="btn">
@@ -73,19 +87,7 @@ const Project = () => {
         </article>
         <article className="portfolio_item">
           <div className="portfolio_item-image">
-            <img src={poke} alt="poke-search-bar" />
-          </div>
-          <h3>project 1</h3>
-          <a href="/" className="btn">
-            gitHub
-          </a>
-          <a href="/" className="btn btn-primary">
-            Live Demo
-          </a>
-        </article>
-        <article className="portfolio_item">
-          <div className="portfolio_item-image">
-            <img src={drum} alt="drum-kit" />
+            <img className="img" src={drum} alt="drum-kit" />
           </div>
           <h3>project 1</h3>
           <a href="/" className="btn">
