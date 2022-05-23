bbd6ed298dea3a10c006244f8f2843cb64249252
leenahiq <97670794+leenahiq@users.noreply.github.com>
2022-05-23 14:14:40 +0100

    updated portfolio page

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,11 +1,13 @@
 import apple from "../assets/apple.png";
 // import recipe from "../assets/recipe.png";
-import insta from "../assets/insta.jpg";
+import insta from "../assets/insta.png";
 import dice from "../assets/dice.png";
 import list from "../assets/list.png";
 import poke from "../assets/poke.png";
 import drum from "../assets/drum.png";
 import "../components/project.css";
+import { BsGithub, BsFillPlayCircleFill } from "react-icons/bs";
+
 const Project = () => {
   return (
     <section id="projects">
@@ -16,132 +18,159 @@ const Project = () => {
             <img className="" src={recipe} alt="recipe site" />
           </div>
           <h3>project 1</h3>
-          <a href="/" className="btn">
-            gitHub
+          <a href="/" className="btn1">
+           <BsGithub className="project-icon"/>
           </a>
-          <a href="/" className="btn btn-primary">
+          <a href="/" className="btn1 ">
             Live Demo
           </a>
         </article> */}
       </div>
       <div className="container portfolio_container">
         <article className="portfolio_item">
+          <h3>Apple Clone</h3>
           <div className="portfolio_item-image">
             <img className="img" src={apple} alt="apple clone" />
           </div>
-          <h3>project 1</h3>
-          <a
-            href="https://github.com/leenahiq/Apple-Clone"
-            target="_blank"
-            rel="noreferrer"
-            className="btn"
-          >
-            gitHub
-          </a>
-          <a
-            href="apple-website-clone-project.netlify.app"
-            className="btn btn-primary"
-          >
-            Live Demo
-          </a>
+          <p>HTML & CSS</p>
+          <div className="portfolio_item-cta">
+            <a
+              href="https://github.com/leenahiq/Apple-Clone"
+              target="_blank"
+              rel="noreferrer"
+              className="btn1"
+            >
+              <BsGithub className="project-icon" />
+            </a>
+            <a href="apple-website-clone-project.netlify.app" className="btn1 ">
+              <BsFillPlayCircleFill className="project-icon" />
+            </a>
+          </div>
         </article>
         <article className="portfolio_item">
+          <h3>Insta Clone</h3>
           <div className="portfolio_item-image">
             <img className="img" src={insta} alt="insta clone" />
           </div>
-          <h3>project 1</h3>
-          <a
-            href="https://github.com/leenahiq/Insta-Clone-React-App"
-            className="btn"
-            target="_blank"
-            rel="noreferrer"
-          >
-            gitHub
-          </a>
-          <a
-            href="https://insta-clone-react-app.netlify.app/"
-            className="btn btn-primary"
-          >
-            Live Demo
-          </a>
+          <p>MERN Stack App</p>
+          <div className="portfolio_item-cta">
+            <a
+              href="https://github.com/leenahiq/Insta-Clone-React-App"
+              className="btn1"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsGithub className="project-icon" />
+            </a>
+            <a
+              href="https://insta-clone-react-app.netlify.app/"
+              className="btn1 "
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsFillPlayCircleFill className="project-icon" />
+            </a>
+          </div>
         </article>
         <article className="portfolio_item">
+          <h3>Dice Roll Game</h3>
           <div className="portfolio_item-image">
             <img className="img" src={dice} alt="dice game" />
           </div>
-          <h3>project 1</h3>
-          <a href="https://github.com/leenahiq/Dice-Roll" className="btn">
-            gitHub
-          </a>
-          <a
-            href=" https://tender-noyce-aedbc2.netlify.app/"
-            className="btn btn-primary"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Live Demo
-          </a>
+          <p>HTML, CSS3 & JavaScript </p>
+          <div className="portfolio_item-cta">
+            <a
+              href="https://github.com/leenahiq/Dice-Roll"
+              className="btn1"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsGithub className="project-icon" />
+            </a>
+            <a
+              href=" https://tender-noyce-aedbc2.netlify.app/"
+              className="btn1 "
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsFillPlayCircleFill className="project-icon" />
+            </a>
+          </div>
         </article>
         <article className="portfolio_item">
+          <h3>TO Do List</h3>
           <div className="portfolio_item-image">
             <img className="img" src={list} alt="to-do-list" />
           </div>
-          <h3>project 1</h3>
-          <a href="https://github.com/leenahiq/TO_DO_LIST" className="btn">
-            gitHub
-          </a>
-          <a
-            href="https://to-do-list-react-app1.netlify.app/"
-            className="btn btn-primary"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Live Demo
-          </a>
+          <p>React.js and CSS3</p>
+          <div className="portfolio_item-cta">
+            <a
+              href="https://github.com/leenahiq/TO_DO_LIST"
+              className="btn1"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsGithub className="project-icon" />
+            </a>
+            <a
+              href="https://to-do-list-react-app1.netlify.app/"
+              className="btn1 "
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsFillPlayCircleFill className="project-icon" />
+            </a>
+          </div>
         </article>
         <article className="portfolio_item">
+          <h3>Poke-Search-Bar</h3>
           <div className="portfolio_item-image">
             <img className="img" src={poke} alt="poke-search-bar" />
           </div>
-          <h3>project 1</h3>
-          <a
-            href="https://github.com/leenahiq/Pokemon-Search-Bar"
-            className="btn"
-            target="_blank"
-            rel="noreferrer"
-          >
-            gitHub
-          </a>
-          <a
-            href="https://poke-search-bar.netlify.app/"
-            className="btn btn-primary"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Live Demo
-          </a>
+          <p>React.js, CSS3 and API</p>
+          <div className="portfolio_item-cta">
+            <a
+              href="https://github.com/leenahiq/Pokemon-Search-Bar"
+              className="btn1"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsGithub className="project-icon" />
+            </a>
+            <a
+              href="https://poke-search-bar.netlify.app/"
+              className="btn1 "
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsFillPlayCircleFill className="project-icon" />
+            </a>
+          </div>
         </article>
         <article className="portfolio_item">
+          <h3>Drum Kit</h3>
           <div className="portfolio_item-image">
             <img className="img" src={drum} alt="drum-kit" />
           </div>
-          <h3>project 1</h3>
-          <a
-            href="https://github.com/leenahiq/Drum-Kit"
-            className="btn"
-            target="_blank"
-            rel="noreferrer"
-          >
-            gitHub
-          </a>
-          <a
-            href="https://quizzical-mestorf-f7282b.netlify.app"
-            className="btn btn-primary"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Live Demo
-          </a>
+          <p>HTML, CSS & JavaScript</p>
+          <div className="portfolio_item-cta">
+            <a
+              href="https://github.com/leenahiq/Drum-Kit"
+              className="btn1"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsGithub className="project-icon" />
+            </a>
+            <a
+              href="https://quizzical-mestorf-f7282b.netlify.app"
+              className="btn1 "
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsFillPlayCircleFill className="project-icon" />
+            </a>
+          </div>
         </article>
       </div>
     </section>
